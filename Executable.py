fa2bf4dfd4873ad6181f287f94a3e78b5212b85e
MattOPI <matthieu.petiteau@gmail.com>
2022-11-05 20:56:01 +0900

    SetUp for multiple games and scores

--- Executable.py
+++ Executable.py
@@ -1,19 +1,41 @@
 import sys
+import os
+
 from ManualPlayer import ManualPlayer
 from pickomino import Pickomino
 
 
 
-root = "./Game1/"
 
 if __name__ == "__main__":
     """Where the game takes place, we create the game and the players and let them play """
 
-    pickomino = Pickomino(2)
-    # p1 = sys.argv[1]
-    p2 = ManualPlayer(pickomino)
-    p3 = ManualPlayer(pickomino)
 
-    players = [p2, p3]
-    while len(pickomino.central_tiles) != 0:
-        pickomino.next_play(players, root)
+    nb_game = 20
+    nb_player = 2
+
+    pickomino = Pickomino(nb_player)
+    p1 = ManualPlayer(pickomino)
+    p2 = ManualPlayer(pickomino)
+
+    players = [p1, p2]
+    score = [0]*nb_player
+    for i in range(nb_game):
+        root = "./Games-1v2/Game" + str(i) + "/"
+        if not os.path.isdir(root):
+            os.makedirs(root)
+
+        # reset
+        pickomino = Pickomino(nb_player)
+        for p in players:
+            p.pickomino = pickomino
+
+        while len(pickomino.central_tiles) != 0:
+            pickomino.next_play(players, root)
+
+        for p in pickomino.winning_player():
+            score[p] +=1
+        print(score)
+
+    print("final score : " + str(score))
+    
